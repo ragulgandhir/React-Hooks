97e2622c7733e96dd4c44a292910eb8c8fb4b3a6
ragulgandhir <ragul.r@hubspire.com>
2022-07-04 18:47:19 +0530

    useEffect

--- src/ReactHooks/useEffectHooks/useEffect.js
+++ src/ReactHooks/useEffectHooks/useEffect.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const UseEffectDemo = () => {
   const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
 
   useEffect(() =>{
     axios.get("https://jsonplaceholder.typicode.com/users")
@@ -10,12 +11,16 @@ const UseEffectDemo = () => {
       setData(response.data[0].email);
       console.log("API WAS CALLED");
     });
-}, []);
+}); // when the state change useEffect hook re-render, empty [] not re-render if listener don't have any component passed
 
   return (
     <div>
-      <h4>useEffectDemo</h4>
-      <h6>Mail ID: {data}</h6>
+      <h1>useEffectDemo</h1>
+      <h1>Count: {count}</h1>
+      <h4>Mail ID: {data}</h4>
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>Increment</button>
     </div>
   )
 }
